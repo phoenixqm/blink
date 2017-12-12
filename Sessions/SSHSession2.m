1a86bfe6b61eda77d337118c82e54e31e3c5023b
Carlos Cabanero <cc@blink.sh>
2017-12-12 11:35:51 -0500

    New SSH client in progress
    - New client
    - libssh as static library

--- Sessions/SSHSession2.m
+++ Sessions/SSHSession2.m
@@ -1,29 +1,408 @@
+#include <getopt.h>
 #include <libssh/libssh.h>
 #include <libssh/callbacks.h>
 #include <sys/time.h>
 
+#import "BKDefaults.h"
+#import "BKHosts.h"
+#import "BKPubKey.h"
 #import "SSHSession2.h"
+
+
+#define REQUEST_TTY_AUTO 0
+#define REQUEST_TTY_NO 1
+#define REQUEST_TTY_YES 2
+#define REQUEST_TTY_FORCE 3
+
+static const char *usage_format =
+  "usage: ssh [options] [user@]hostname [command]\r\n"
+  "[-l login_name] [-i identity_file] [-p port]\r\n" 
+  "[-t request_tty] [-v verbose]\r\n"
+  "\r\n";
+
+typedef struct {
+  int address_family;
+  int connection_timeout;
+  int port;
+  const char *hostname;
+  const char *user;
+  int request_tty;
+  const char *identity_file;
+  const char *password;
+  BOOL disableHostKeyCheck;
+  const char *command;
+} session_options;
 
 @interface SSHSession2 ()
 @end
 
 void loggingEvent(ssh_session session, int priority, const char *message, void *userdata)
 {
-  printf("%s\n", message);
+  //printf("%s\n", message);
 }
 
 struct ssh_callbacks_struct cb = {
   .userdata = NULL,
-  .auth_function = NULL,
-  .log_function = loggingEvent
+  .auth_function = NULL
+  //.log_function = loggingEvent
 };
 
 @implementation SSHSession2 {
+  session_options _options;
   ssh_session _session;
-  ssh_channel _channel;
+  ssh_channel _channel;  
+}
+
+- (int)usage
+{
+  return [self dieMsg:[NSString stringWithFormat:@"%s", usage_format]];
 }
 
 - (int)main:(int)argc argv:(char **)argv
+{
+  if ([self opts:argc argv:argv] < 0) {
+    return [self usage];
+  }
+
+  _session = ssh_new();
+  
+  if (!ssh_is_connected(_session)) {
+    [self debugMsg:@"Yo!"];
+  }
+  
+  [self client];
+
+  // Process options or take from settings
+
+  // Apply config to session
+
+  // Connect & validate server
+
+  // Login - no auto. Read key manually, etc... identity separated.
+  // Can we set ssh_key somewhere else? No, just through no auto.
+  return 0;
+}
+
+- (int)opts:(int)argc argv:(char **)argv
+{
+    // Options
+  // port -p
+  // verbose --verbose
+  // command (Obtain data at the end)
+  // tty -t
+  // key -i identity file
+  // forced password
+  optind = 1;
+
+  while (1) {
+    int c = getopt_long(argc, argv, "p:i:htvl:", NULL, NULL);
+    if (c == -1) {
+      break;
+    }
+    char *ep;
+    switch (c) {
+      case 'p':
+	_options.port = (unsigned int)strtol(optarg, &ep, 10);
+	if (optarg == ep || *ep != '\0' || _options.port > 65536) {
+	  return [self dieMsg:@"Wrong port value provided."];
+	}
+	break;
+      case 'h':
+        _options.disableHostKeyCheck = true;
+        break;
+      case 'v':
+	_debug = 1;
+	break;
+      case 'i':
+	_options.identity_file = optarg;
+	break;
+      case 't':
+	_options.request_tty = REQUEST_TTY_FORCE;
+	break;
+      case 'l':
+	_options.user = optarg;
+	break;
+      default:
+	optind = 0;
+	return [self dieMsg:@(usage_format)];
+    }
+  }
+
+  if (argc - optind < 1) {
+    return [self dieMsg:@(usage_format)];
+  }
+
+  BKHosts *savedHost;
+  NSArray *userAtHost = [[NSString stringWithFormat:@"%s", argv[optind++]]
+			  componentsSeparatedByString:@"@"];
+  char **command_args = &argv[optind];
+  int num_command_args = argc - optind;
+
+  if ([userAtHost count] < 2) {
+    _options.hostname = [userAtHost[0] UTF8String];
+  } else {
+    _options.user = [userAtHost[0] UTF8String];
+    _options.hostname = [userAtHost[1] UTF8String];
+  }
+  
+  if ((savedHost = [BKHosts withHost:[NSString stringWithFormat:@"%s", _options.hostname]])) {
+    _options.hostname = savedHost.hostName ? [savedHost.hostName UTF8String] : _options.hostname;
+    _options.port = _options.port ? _options.port : [savedHost.port intValue];
+    if (!_options.user && [savedHost.user length]) {
+      _options.user = [savedHost.user UTF8String];
+    }
+    _options.identity_file = _options.identity_file ? _options.identity_file : [savedHost.key UTF8String];
+    _options.password = savedHost.password ? [savedHost.password UTF8String] : NULL;
+  }
+  
+  if (num_command_args) {
+    NSString *command = [NSString stringWithFormat:@"%s", command_args[0]];
+
+    for (int i = 1; i < num_command_args; i++) {
+      NSString *arg = [NSString stringWithFormat:@" %s", command_args[i]];
+      command = [command stringByAppendingString:arg];
+    }
+    _options.command = [command UTF8String];
+  } else {
+    _options.request_tty = REQUEST_TTY_YES;
+  }
+  return 0;
+}
+
+- (int)client
+{
+  if (!ssh_is_connected(_session)) {
+    [self debugMsg:@"Yo!"];
+  }
+  
+  [self setSessionOptions];
+  
+  if (![self verifyKnownHost]) {
+    return [self dieMsg:@"Host key verification failed"];
+  }
+
+  int rc = ssh_connect(_session);
+  if (rc != SSH_OK) {
+    // TODO: free on die? how were we doing this before? How about on cleanup of the object?    
+    return [self dieMsg:@"Error connecting to HOST"];
+  }
+  
+  ssh_userauth_none(_session, NULL);
+  char *banner = ssh_get_issue_banner(_session);
+  if (banner) {
+    fprintf(_stream.out, "%s\r\n", banner);
+    free(banner);
+  }
+  
+  if (![self authenticate]) {
+    return [self dieMsg:@"Authentication error"];
+  }
+  
+  // if (_options.tty_flag) {
+  //   [self shell];
+  // } else {
+  //   // exec
+  // }
+  return 0;
+}
+
+- (int)setSessionOptions
+{
+  if (ssh_options_set(_session, SSH_OPTIONS_HOST, _options.hostname) < 0) {
+    return [self dieMsg:@"Error setting Host"];
+  }
+
+  if (ssh_options_set(_session, SSH_OPTIONS_USER, _options.user) < 0) {
+    return [self dieMsg:@"Error setting user"];
+  }
+
+  if (_options.port && ssh_options_set(_session, SSH_OPTIONS_PORT, &_options.port) < 0) {
+    return [self dieMsg:@"Error setting port"];
+  }
+  
+  return 0;
+}
+
+- (int)verifyKnownHost
+{
+  // TODO: Creating the file where we want, modifying entries, etc...
+  return 1;
+}
+
+- (int)authenticate
+{
+  int rc;
+  int method;
+  char *password;
+  char *banner;
+  
+  rc = ssh_userauth_none(_session, NULL);
+  if (rc == SSH_AUTH_ERROR) {
+    return rc;
+  }
+  
+  method = ssh_userauth_list(_session, NULL);
+  while (rc != SSH_AUTH_SUCCESS) {
+    // if (method & SSH_AUTH_METHOD_GSSAPI_MIC){
+    //   rc = ssh_userauth_gssapi(session);
+    //   if(rc == SSH_AUTH_ERROR) {
+    // 	error(session);
+    // 	return rc;
+    //   } else if (rc == SSH_AUTH_SUCCESS) {
+    // 	break;
+    //   }
+    // }
+    // Try to authenticate with public key first
+    if (method & SSH_AUTH_METHOD_PUBLICKEY) {
+      rc = ssh_userauth_publickey_auto(_session, NULL, NULL);
+      if (rc == SSH_AUTH_ERROR) {
+	// TODO: Print Error message
+	// [sshError @"Authentication failed: "]
+	//error(session);
+	return rc;
+      } else if (rc == SSH_AUTH_SUCCESS) {
+        break;
+      }
+    }
+
+    // Try to authenticate with keyboard interactive";
+    if (method & SSH_AUTH_METHOD_INTERACTIVE) {
+      // TODO: Pass initial password from default
+      rc = [self authInteractive:NULL];
+      if (rc == SSH_AUTH_ERROR) {
+      	//error(session);
+        return rc;
+      } else if (rc == SSH_AUTH_SUCCESS) {
+        break;
+      }
+    }
+
+    fprintf(_stream.out, "Password: ");
+    if ([self promptUser:&password] < 0) {
+      return SSH_AUTH_ERROR;
+    }
+
+    // Try to authenticate with password
+    if (method & SSH_AUTH_METHOD_PASSWORD) {
+      rc = ssh_userauth_password(_session, NULL, password);
+      if (rc == SSH_AUTH_ERROR) {
+      	//error(session);
+        return rc;
+      } else if (rc == SSH_AUTH_SUCCESS) {
+        break;
+      }
+    }
+    memset(password, 0, sizeof(password));
+  }
+
+  banner = ssh_get_issue_banner(_session);
+  if (banner) {
+    fprintf(_stream.out, "%s\r\n", banner);
+    ssh_string_free_char(banner);
+  }
+
+  return rc;
+}
+
+- (int)authInteractive:(const char *)password
+{
+  int err;
+  FILE *out = _stream.out;
+  
+  err = ssh_userauth_kbdint(_session, NULL, NULL);
+  while (err == SSH_AUTH_INFO) {
+    const char *instruction;
+    const char *name;
+    char *buffer;
+    int i, n;
+
+    name = ssh_userauth_kbdint_getname(_session);
+    instruction = ssh_userauth_kbdint_getinstruction(_session);
+    n = ssh_userauth_kbdint_getnprompts(_session);
+
+    if (name && strlen(name) > 0) {
+      fprintf(out, "%s\r\n", name);
+    }
+
+    if (instruction && strlen(instruction) > 0) {
+      fprintf(out, "%s\r\n", instruction);
+    }
+
+    for (i = 0; i < n; i++) {
+      const char *answer;
+      const char *prompt;
+      char echo;
+
+      prompt = ssh_userauth_kbdint_getprompt(_session, i, &echo);
+      if (prompt == NULL) {
+	break;
+      }
+
+      if (echo) {
+        fprintf(out, "%s", prompt);
+        [self promptUser: &buffer];
+	//char *p;
+
+	// if (fgets(buffer, sizeof(buffer), _stream.in) == NULL) {
+	//   return SSH_AUTH_ERROR;
+	// }
+
+	// buffer[sizeof(buffer) - 1] = '\0';
+	// if ((p = strchr(buffer, '\n'))) {
+	//   *p = '\0';p
+	// }
+
+	if (ssh_userauth_kbdint_setanswer(_session, i, buffer) < 0) {
+	  return SSH_AUTH_ERROR;
+	}
+
+	memset(buffer, 0, strlen(buffer));
+      } else {
+	if (password && strstr(prompt, "Password:")) {
+	  answer = password;
+	} else {
+	  buffer[0] = '\0';
+
+	  fprintf(out, "%s", prompt);
+	  if ([self promptUser:buffer] < 0) {
+	    return SSH_AUTH_ERROR;
+	  }
+	  // if (ssh_getpass(prompt, buffer, sizeof(buffer), 0, 0) < 0) {
+	  //   return SSH_AUTH_ERROR;
+	  // }
+	  answer = buffer;
+	}
+	err = ssh_userauth_kbdint_setanswer(_session, i, answer);
+	memset(buffer, 0, sizeof(buffer));
+	if (err < 0) {
+	  return SSH_AUTH_ERROR;
+	}
+      }
+    }
+    err=ssh_userauth_kbdint(_session,NULL,NULL);
+  }
+
+  return err;
+}
+
+- (ssize_t)promptUser:(char **)resp
+{
+  //char *line=NULL;
+  size_t size = 0;
+  ssize_t sz = 0;
+
+  FILE *termin = _stream.in;
+  if ((sz = getdelim(resp, &size, '\r', termin)) == -1) {
+    return -1;
+  } else {
+    if ((*resp)[sz - 1] == '\r') {
+      (*resp)[--sz] = '\0';
+    }
+    return sz;
+  }
+}
+
+- (int)old_main:(int)argc argv:(char **)argv
 {
   _session = ssh_new();
   if (_session == NULL) {
@@ -37,13 +416,13 @@ struct ssh_callbacks_struct cb = {
     [self debugMsg:@"Yo!"];
   }
   // TODO: Configure session
-  if (ssh_options_set(_session, SSH_OPTIONS_HOST, "192.168.128.109") < 0) {
+  if (ssh_options_set(_session, SSH_OPTIONS_HOST, _options.hostname) < 0) {
     [self dieMsg:@"Error setting host"];
   }
-  if (ssh_options_set(_session, SSH_OPTIONS_USER, "carlos") < 0) {
-    [self dieMsg:@"Error setting host"];
+  if (ssh_options_set(_session, SSH_OPTIONS_USER, _options.user) < 0) {
+    [self dieMsg:@"Error setting user"];
   }    
-  //ssh_options_set(_session, SSH_OPTIONS_PORT, &port);
+  ssh_options_set(_session, SSH_OPTIONS_PORT, &_options.port);
   // int verbosity = SSH_LOG_PROTOCOL;
   // ssh_options_set(_session, SSH_OPTIONS_LOG_VERBOSITY, &verbosity);
   // TODO:Log verbosity with callback.
@@ -95,6 +474,43 @@ struct ssh_callbacks_struct cb = {
   ssh_free(_session);
   return 0;
 }
+
+- (void)processUserHostSettings:(NSString *)userhost
+{
+  BKHosts *savedHost;
+
+  NSArray *chunks = [userhost componentsSeparatedByString:@"@"];
+  NSString *hostname, *user;
+
+  if ([chunks count] < 2) {
+    hostname = chunks[0];
+    _options.hostname = [hostname UTF8String];
+  } else {
+    user = chunks[0];
+    hostname = chunks[1];
+    _options.hostname = [hostname UTF8String];
+    _options.user = [user UTF8String];
+  }
+
+  if (!(savedHost = [BKHosts withHost:hostname])) {
+    return;
+  }
+
+  _options.hostname = savedHost.hostName ? [savedHost.hostName UTF8String] : _options.hostname;
+  _options.port = _options.port ? _options.port : [savedHost.port intValue];
+
+  if (!_options.user && [savedHost.user length]) {
+    _options.user = [savedHost.user UTF8String];
+  }
+  _options.identity_file = _options.identity_file ? _options.identity_file : [savedHost.key UTF8String];
+  _options.password = savedHost.password ? [savedHost.password UTF8String] : NULL;
+  
+  if (!_options.user) {
+    // If no user provided, use the default
+    _options.user = [[BKDefaults defaultUserName] UTF8String];
+  }
+}
+
 
 - (void)loop
 {
@@ -158,6 +574,7 @@ struct ssh_callbacks_struct cb = {
 
 - (void)sigwinch
 {
+  // TODO: Fails when changing apps, etc... if there is no app yet.
   ssh_channel_change_pty_size(_channel,
 			      _stream.sz->ws_col, 
 			      _stream.sz->ws_row);
